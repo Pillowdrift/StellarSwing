63993032a1a01a8eed9a7a44e29a6b9c99b18a7e
Caitlin Wilks <c.wilks@virtalis.com>
2020-05-06 02:57:26 +0100

    add new splash and title screen

---
+++ Assets/Assets_new/ImageFader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageFader : MonoBehaviour
+{
+  private Image image;
+
+  void Awake()
+  {
+    image = GetComponent<Image>();
+    image.enabled = true;
+    StartCoroutine(FadeIn());
+  }
+
+  IEnumerator FadeIn()
+  {
+    yield return new WaitForSeconds(0.5f);
+    image.CrossFadeAlpha(0.1f, 0.4f, false);
+  }
+}
