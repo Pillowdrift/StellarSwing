3ee9da62a8706524e18ca314332744ceb96d9678
Caitlin Wilks <c.wilks@virtalis.com>
2020-05-08 16:42:24 +0100

    add hats to drone and a random hat selector on title screen which is currently disabled

---
+++ Assets/Assets_new/RandomHatSelector.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomHatSelector : MonoBehaviour
+{
+  void Start()
+  {
+    int hatToEnable = Random.Range(0, transform.childCount);
+    transform.GetChild(hatToEnable).gameObject.SetActive(true);
+  }
+}
