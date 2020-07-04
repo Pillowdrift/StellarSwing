54d1d51ceed2c8d96d12412e66c5d8132af8d3db
Caitlin Wilks <c.wilks@virtalis.com>
2020-07-04 21:44:58 +0100

    - fix ore cubes - revert world 1 levels back to clean state - insert story into other worlds

---
+++ Assets/Assets_new/SpeedBooster.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedBooster : MonoBehaviour
+{
+  public float Power = 1.0f;
+
+  private GameObject boostingObject;
+
+  void FixedUpdate()
+  {
+    if (boostingObject != null)
+    {
+      var direction = boostingObject.transform.forward;
+      boostingObject.GetComponent<Rigidbody>().AddForce(direction * Power);
+    }
+  }
+
+  void OnTriggerEnter(Collider other)
+  {
+    if (other.name == "Player")
+    {
+      boostingObject = other.gameObject;
+    }
+  }
+
+  void OnTriggerExit(Collider other)
+  {
+    if (other.gameObject == boostingObject)
+    {
+      boostingObject = null;
+    }
+  }
+}
