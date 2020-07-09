88e8ced02ff3858bfe16bee068ae02c636fc421d
olive <lucioliviagray@gmail.com>
2020-07-09 09:43:36 -0400

    world completion unlocks

--- Assets/AchievementUnlocker.cs
+++ Assets/AchievementUnlocker.cs
@@ -1,21 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
+//using System.Diagnostics;
 using UnityEngine;
+using Steamworks;
 
-public class AchievementUnlocker : MonoBehaviour
-{
+public class AchievementUnlocker : MonoBehaviour {
   public string AchievementIDStr;
+  public bool UnlockOnLoad = false;
+  private bool unlocked;
 
   void Awake() {
-    if (!SteamManager.Initialized) {
+    CheckActive();
+  }
+
+  bool CheckActive() {
+    if (!SteamManager.Initialized || AchievementIDStr == null) {
       gameObject.SetActive(false);
+      if (AchievementIDStr == null) Debug.LogError("AchievementIDStr not set in AchievementUnlocker!");
+      return false;
+    }
+    return true;
+  }
+
+
+  void UnlockAchievement() {
+    if (!CheckActive()) return;
+    SteamUserStats.GetAchievement(AchievementIDStr, out unlocked);
+    if (!unlocked) {
+      SteamUserStats.SetAchievement(AchievementIDStr);
+      SteamUserStats.StoreStats();
+    } else {
+      Debug.Log("Already unlocked " + AchievementIDStr);
     }
   }
 
   // Start is called before the first frame update
   void Start() {
-    
+    if (UnlockOnLoad) {
+      UnlockAchievement();
+    }
   }
 
   // Update is called once per frame
